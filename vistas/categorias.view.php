d1f0c6d36cb4eccd67103313a8629a2fb648e2c0
unknown <linuxlite20@gmail.com>
2020-05-06 16:12:48 +0200

    cambios en equipos del css y la view

--- vistas/categorias.view.php
+++ vistas/categorias.view.php
@@ -4,21 +4,8 @@
     <div id="divListaEquipos">
       <div id="listaEquipos">
         <h3>Equipos de la liga</h3>
-        <ul>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
-          <li><i class="fas fa-futbol"></i>real madrid</li>
+        <ul id="laLista">
+          
         </ul>
       </div>
     </div><!-- fin de ListaEquipos-->
@@ -30,37 +17,8 @@
         <a href="marca.php"><p>la liga te espera ..</p></a>
       </div>
 
-      <div class="paginacion" id="paginacionS">
-        <div id="izqS" value="is" name="btnPagNum"><<</div>
-        <div id="paginaNumerosS" class="numPag"></div>
-        <div id="derS" value="ds"  name="btnPagNum">>></div>
-      </div>
-
-      <div id="equipo">
-        <h2>nombre Equipo</h2>
-        <div class="divEquipo">
-          <div class="imagenEquipo">
-            <img src="imagenes/equipos/equipo1.jpeg" alt="">
-          </div>
-          <div class="divLema">
-            <p>
-              lema del equipo blavlaloijoijgoifjdfisofijsoifjoslema del
-              equipo blavlaloijoijgoifjdfisofijsoifjoslema del equipo
-              blavlaloijoijgoifjdfisofijsoifjoslema del equipo blavlal
-              oijoijgoifjdfisofijsoifjoslema del equipo blavlaloijoijgo
-              ifjdfisofijsoifjoslema del equipo blavlaloijoijgoifjdfiso
-            
-            </p>
-            <button>Conoce mas del equipo</button>
-            <button>Unirme al equipo!!</button>
-          </div>
-        </div>
-      </div>
-    
-      <div class="paginacion" id="paginacionP">
-        <div id="izqP" value="ip" name="btnPagNum"><<</div>
-        <div id="paginaNumerosP" class="numPag"></div>
-        <div id="derP" value="dp"  name="btnPagNum">>></div>
+      <div id="padreEquipos">
+      
       </div>
 
       <button id="up" class="up"><i class="far fa-arrow-alt-circle-up"></i></button>
